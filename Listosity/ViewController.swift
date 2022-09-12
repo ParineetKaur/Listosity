b26b452cf8f8bc55a182cc79b4347293e38a5c2a
Parneet Kaur <parneetk03@gmail.com>
2022-09-12 11:08:24 -0700

    Initial Commit

---
+++ Listosity/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Listosity
+//
+//  Created by Parneet Kaur on 9/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
